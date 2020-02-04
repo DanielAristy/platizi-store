a9c105dad4092e068a6845b0e31a30e4b0e13f7b
DanielAristy <danielariaty@gmail.com>
2020-02-04 17:34:37 -0500

    Empezando con la app de Tienda web

---
+++ src/app/product.model.ts
@@ -0,0 +1,8 @@
+/**Representacion clara de un producto en la tienda */
+export interface Product{
+    id: string;
+    image: string;
+    title: string;
+    price: number;
+    description: string;
+}
